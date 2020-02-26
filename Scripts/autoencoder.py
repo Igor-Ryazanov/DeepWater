b0db888c9553bde2fd574e543fcf19d7bf02c028
unknown <rzigor14@gmail.com>
2020-02-26 14:01:21 +0100

    Structure Update

--- Scripts/autoencoder.py
+++ Scripts/autoencoder.py
@@ -4,71 +4,73 @@ from torch import optim
 from torch.utils.data import DataLoader
 import numpy as np
 
-#Default CPU for now
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class Autoencoder(nn.Module):
-    def __init__(self):
-        super(Autoencoder, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv2d(4, 16, kernel_size = 3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(16, 32, kernel_size = 3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size = 7)
-        )
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, kernel_size = 7),
-            nn.ReLU(),
-            nn.ConvTranspose2d(32, 16, kernel_size = 3, stride=2, padding=1, output_padding=1),
-            nn.ReLU(),
-            nn.ConvTranspose2d(16, 4, kernel_size = 3, stride=2, padding=1, output_padding=1),
-            nn.Sigmoid()
-        )
+	def __init__(self):
+		super(Autoencoder, self).__init__()
+		self.encoder = nn.Sequential(
+			nn.Conv2d(4, 16, kernel_size = 3, stride=2, padding=1),
+			nn.ReLU(),
+			nn.Conv2d(16, 32, kernel_size = 3, stride=2, padding=1),
+			nn.ReLU(),
+			nn.Conv2d(32, 64, kernel_size = 7)
+		)
+		self.decoder = nn.Sequential(
+			nn.ConvTranspose2d(64, 32, kernel_size = 7),
+			nn.ReLU(),
+			nn.ConvTranspose2d(32, 16, kernel_size = 3, stride=2, padding=1, output_padding=1),
+			nn.ReLU(),
+			nn.ConvTranspose2d(16, 4, kernel_size = 3, stride=2, padding=1, output_padding=1),
+			nn.Sigmoid()
+		)
 
-    def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+	def forward(self, x):
+		x = self.encoder(x)
+		x = self.decoder(x)
+		return x
 
 
 
 def train_autoencoder(model, train_data, num_epochs=5, batch_size=5, learning_rate=1e-3):
-    torch.manual_seed(0)
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
-    train_data = torch.from_numpy(train_data)
-    train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size, num_workers=4, drop_last=True)
-    outputs = []
-    for epoch in range(num_epochs):
-        for data in train_loader:
-            rec = model(data)
-            loss = criterion(rec, data)
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
+	#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	model = model.to(device)
+	torch.manual_seed(0)
+	criterion = nn.MSELoss()
+	optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+	train_data = torch.from_numpy(train_data)
+	train_data = train_data.to(device)
+	train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size, num_workers=0, drop_last=True)
+	outputs = []
+	for epoch in range(num_epochs):
+		for data in train_loader:
+			rec = model(data)
+			loss = criterion(rec, data)
+			loss.backward()
+			optimizer.step()
+			optimizer.zero_grad()
 
-        print('Epoch:{}, Loss:{:.4f}'.format(epoch+1, float(loss)))
-        outputs.append((epoch, data, rec),)
-    return outputs
+		print('Epoch:{}, Loss:{:.4f}'.format(epoch+1, float(loss)))
+		outputs.append((epoch, data, rec),)
+	return outputs
 
 def test_reconstruction(model, test_frame):
-	test_reconst = model(torch.from_numpy(test_frame).view([1, 4, 28, 60]))
-	return test_reconst.detach().numpy().reshape(4,28,60)
+	test_reconst = model(torch.from_numpy(test_frame).to(device).view([1, 4, 28, 60]))
+	return test_reconst.cpu().detach().numpy().reshape(4,28,60)
 
 def compress_wakes(wakes, model):
 	compressed_wakes = []
 	for wake in wakes:
-		wake_image = torch.from_numpy(wake[:,:-1,:]).view([1, 4, 28, 60])
-		compressed_wake = model.encoder(wake_image).detach().numpy()
+		wake_image = torch.from_numpy(wake[:,:-1,:]).to(device).view([1, 4, 28, 60])
+		compressed_wake = model.encoder(wake_image).cpu().detach().numpy()
 		compressed_wakes.append(compressed_wake)
 	return np.array(compressed_wakes)
 	
 def decompress_wakes(seeds, model):
 	decompressed_wakes = []
 	for seed in seeds:
-		seed_tensor = torch.from_numpy(seed).view([1, 64, 1, 9])
-		decompressed_wake = model.decoder(seed_tensor).detach().numpy().reshape(4, 28, 60)
+		seed_tensor = torch.from_numpy(seed).to(device).view([1, 64, 1, 9])
+		decompressed_wake = model.decoder(seed_tensor).cpu().detach().numpy().reshape(4, 28, 60)
 		decompressed_wakes.append(decompressed_wake)
 	return np.array(decompressed_wakes)
--- Scripts/wake_processing.py
+++ Scripts/wake_processing.py
@@ -6,107 +6,143 @@ import numpy as np
 import pandas as pd
 
 def make_heatmap(im, dt, cbar = False):
-    sns.heatmap(im, cmap = 'coolwarm', cbar = cbar)
-    if dt is None:
-        new_title = 'No date'
-    else:
-        dt = stamp_to_date(int(dt[0]))
-        new_title = str(dt[1])+'-'+str(dt[0])+'-2018 '+ str(dt[2])+ ':'+ str(dt[3])
-    plt.title(new_title)
-    plt.yticks(np.arange(28, step = 4),np.arange(3, 30, step = 4))
-    xinterval= np.timedelta64(30,'s')
-    plt.xticks(np.arange(im.shape[1], step = 10), [(xinterval*i) / np.timedelta64(1, 'm') for i in np.arange(im.shape[1], step = 10)])
-    plt.xlabel("Time (minutes)")
-    plt.ylabel("Depth")
-    plt.ioff()
-    plt.show()
-            
+	sns.heatmap(im, cmap = 'coolwarm', cbar = cbar)
+	if dt is None:
+		new_title = 'No date'
+	else:
+		dt = stamp_to_date(int(dt[0]))
+		new_title = str(dt[1])+'-'+str(dt[0])+'-2018 '+ str(dt[2])+ ':'+ str(dt[3])
+	plt.title(new_title)
+	plt.yticks(np.arange(28, step = 4),np.arange(3, 30, step = 4))
+	xinterval= np.timedelta64(30,'s')
+	plt.xticks(np.arange(im.shape[1], step = 10), [(xinterval*i) / np.timedelta64(1, 'm') for i in np.arange(im.shape[1], step = 10)])
+	plt.xlabel("Time (minutes)")
+	plt.ylabel("Depth")
+	plt.ioff()
+	plt.show()
+			
 def make_12_heatmaps(im, title = 'Wake Samples', cbar = False, save = False, filename = None):
-    fig, axs = plt.subplots(4, 3, figsize=(25,15))
-    fig.suptitle(title, fontweight="bold", size=20)
-    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-    for i in range(4):
-        for j in range(3):
-            ax = axs[i,j]
-            sns.heatmap(im[i*3+j], cmap = 'coolwarm', ax = ax, cbar = cbar)            
-    xinterval = np.timedelta64(30,'s')
-    plt.setp(axs, xticks=np.arange(im.shape[2], step = 10), 
-             xticklabels=[(xinterval*j) / np.timedelta64(1, 'm') for j in np.arange(im.shape[2], step = 10)],
-             yticks=np.arange(28, step = 4), yticklabels = np.arange(3, 30, step = 4))
-    fig.text(0.45, 0.01, 'Time (minutes)', ha='center',  size=20)
-    fig.text(-0.01, 0.5, 'Depth (meters)', va='center', rotation='vertical',  size=20)
-    if save:
-        plt.savefig(filename)
-    plt.show()
+	fig, axs = plt.subplots(4, 3, figsize=(25,15))
+	fig.suptitle(title, fontweight="bold", size=20)
+	fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+	for i in range(4):
+		for j in range(3):
+			ax = axs[i,j]
+			sns.heatmap(im[i*3+j], cmap = 'coolwarm', ax = ax, cbar = cbar)            
+	xinterval = np.timedelta64(30,'s')
+	plt.setp(axs, xticks=np.arange(im.shape[2], step = 10), 
+			 xticklabels=[(xinterval*j) / np.timedelta64(1, 'm') for j in np.arange(im.shape[2], step = 10)],
+			 yticks=np.arange(28, step = 4), yticklabels = np.arange(3, 30, step = 4))
+	fig.text(0.45, 0.01, 'Time (minutes)', ha='center',  size=20)
+	fig.text(-0.01, 0.5, 'Depth (meters)', va='center', rotation='vertical',  size=20)
+	if save:
+		plt.savefig(filename)
+	plt.show()
 	
 
 def stamp_to_date(st):
-    month = 8
-    day = st // (3600*24) + 28
-    hour = st % (3600*24) // 3600 + 11
-    mins = st % (3600*24) % 3600 / 60
-    if hour >= 24:
-        hour -= 24
-        day += 1
-    if day > 31:
-        day -= 31
-        month +=1
-    return month, day, hour, mins
+	month = 8
+	day = st // (3600*24) + 28
+	hour = st % (3600*24) // 3600 + 11
+	mins = st % (3600*24) % 3600 / 60
+	if hour >= 24:
+		hour -= 24
+		day += 1
+	if day > 31:
+		day -= 31
+		month +=1
+	return month, day, hour, mins
 	
 
 def date_to_stamp(dt):
-    month, day, hour, minute = dt
-    stamp = 0
-    if month == 9:
-        stamp += 24*3600*4
-        stamp -= 11*3600
-        stamp += (day-1)*24*3600
-        stamp += (hour)*3600
-        stamp += (minute)*60
-    if month == 8:
-        stamp += (day-28)*24*3600
-        stamp += (hour-11)*3600
-        stamp += (minute)*60
-    return stamp
+	month, day, hour, minute = dt
+	stamp = 0
+	if month == 9:
+		stamp += 24*3600*4
+		stamp -= 11*3600
+		stamp += (day-1)*24*3600
+		stamp += (hour)*3600
+		stamp += (minute)*60
+	if month == 8:
+		stamp += (day-28)*24*3600
+		stamp += (hour-11)*3600
+		stamp += (minute)*60
+	return stamp
 	
 def frames_by_timestamps(data_series, wake_start_ts):
-    wake_samples = []
-    for ws in wake_start_ts:
-        ind = np.where(data_series[:,-1,:]==ws)[1]
-        if ind.size !=0:
-            ind2 = int(ind[0])+60
-            wake_samples.append(data_series[:,:,int(ind[0]):ind2])
-    wake_samples = np.array(wake_samples)
-    return wake_samples
+	wake_samples = []
+	for ws in wake_start_ts:
+		ind = np.where(data_series[:,-1,:]==ws)[1]
+		if ind.size !=0:
+			ind2 = int(ind[0])+60
+			wake_samples.append(data_series[:,:,int(ind[0]):ind2])
+	wake_samples = np.array(wake_samples)
+	return wake_samples
 	
 def get_files():
+	m = ['08_']*8 + ['09_']*49
+	d_n = ['day_', 'night_']*28 + ['day_']
 	path = 'Data\\Matlab files'
-	files = sorted(os.listdir(path), key=lambda x: os.path.getmtime(os.path.join(path, x)))
-	full_data_5 = loadmat(path + '\\day_08_28_pythonfile.mat')["a5m"]
-	full_data_4 = loadmat(path +'\\day_08_28_pythonfile.mat')["a4m"]
-	full_data_3 = loadmat(path + '\\day_08_28_pythonfile.mat')["a3m"]
-	full_data_1 = loadmat(path + '\\day_08_28_pythonfile.mat')["a1m"]
-	full_times = loadmat(path + '\\day_08_28_pythonfile.mat')['tm']
+	day_list = []
+	for i in range(28, 31+26):
+		if i > 31:
+			dd =  i % 32 + 1
+			if dd < 10:
+				dd = '0' + str(dd)
+			else: dd = str(dd)
+		else:
+			dd = str(i % 32)
+		day_list.append(dd)
+	d = []
+	yesterday = '__'
+	for today in day_list:
+		d.append(yesterday+'_'+today)
+		d.append(today)
+		yesterday = today
+	d = d[1:]
+	file_names = []
+	for i in range(57):
+		file_name = d_n[i]+m[i]+d[i]+'_pythonfile.mat'
+		file_names.append(file_name)
+	full_data_5 = loadmat(path + "\\" + file_names[0])["a5m"]
+	full_data_4 = loadmat(path + "\\" + file_names[0])["a4m"]
+	full_data_3 = loadmat(path + "\\" + file_names[0])["a3m"]
+	full_data_1 = loadmat(path + "\\" + file_names[0])["a1m"]
+	full_times = loadmat(path + "\\" + file_names[0])['tm']
 	start_time = full_times[0,0]
 	full_times = np.round((full_times - start_time)*24*3600)
-	for file in files:
-		if file.endswith("pythonfile.mat"):
-			if file != 'day_08_28_pythonfile.mat':
-				mat_file = loadmat(path + "\\" + file)
-				full_data_5 = np.append(full_data_5, mat_file["a5m"], axis = 0)
-				full_data_4 = np.append(full_data_4, mat_file["a4m"], axis = 0)
-				full_data_3 = np.append(full_data_3, mat_file["a3m"], axis = 0)
-				full_data_1 = np.append(full_data_1, mat_file["a1m"], axis = 0)
-				# Convert timestamps from days to secs
-				# Assuming first stamp to be from 11.00, 28 August 2018
-				times = np.round((mat_file["tm"] - start_time)*24*3600)
-				full_times = np.append(full_times, times)
+	day_night = np.ones(full_times.shape) # 1 - day, 0 - night
+	day_count = full_times.shape[1]
+	night_count = 0
+	for i, file in enumerate(file_names[1:]):
+		mat_file = loadmat(path + "\\" + file)
+		full_data_5 = np.append(full_data_5, mat_file["a5m"], axis = 0)
+		full_data_4 = np.append(full_data_4, mat_file["a4m"], axis = 0)
+		full_data_3 = np.append(full_data_3, mat_file["a3m"], axis = 0)
+		full_data_1 = np.append(full_data_1, mat_file["a1m"], axis = 0)
+		# Convert timestamps from days to secs
+		# Assuming first stamp to be from 11.00, 28 August 2018
+		times = np.round((mat_file["tm"] - start_time)*24*3600)
+		full_times = np.append(full_times, times)
+		if i % 2 == 0:
+			night_count += times.shape[1]
+			dn = np.zeros(times.shape)
+		else:
+			day_count += times.shape[1]
+			dn = np.ones(times.shape)
+		day_night = np.append(day_night, dn)
 	max_response = np.array([full_data_1, full_data_3, full_data_4, full_data_5]).max()
-	data_series_5 = np.append(np.flip(full_data_5, 1)/max_response, full_times.reshape(full_times.shape[0], 1), axis = 1).transpose()
-	data_series_4 = np.append(np.flip(full_data_4, 1)/max_response, full_times.reshape(full_times.shape[0], 1), axis = 1).transpose()
-	data_series_3 = np.append(np.flip(full_data_3, 1)/max_response, full_times.reshape(full_times.shape[0], 1), axis = 1).transpose()
-	data_series_1 = np.append(np.flip(full_data_1, 1)/max_response, full_times.reshape(full_times.shape[0], 1), axis = 1).transpose()
+	data_series_5 = np.append(np.flip(full_data_5, 1)/max_response, full_times.reshape(full_times.shape[0], 1), axis = 1)
+	data_series_4 = np.append(np.flip(full_data_4, 1)/max_response, full_times.reshape(full_times.shape[0], 1), axis = 1)
+	data_series_3 = np.append(np.flip(full_data_3, 1)/max_response, full_times.reshape(full_times.shape[0], 1), axis = 1)
+	data_series_1 = np.append(np.flip(full_data_1, 1)/max_response, full_times.reshape(full_times.shape[0], 1), axis = 1)
+	data_series_5 = np.append(data_series_5, day_night.reshape(day_night.shape[0], 1), axis = 1).transpose()
+	data_series_4 = np.append(data_series_4, day_night.reshape(day_night.shape[0], 1), axis = 1).transpose()
+	data_series_3 = np.append(data_series_3, day_night.reshape(day_night.shape[0], 1), axis = 1).transpose()
+	data_series_1 = np.append(data_series_1, day_night.reshape(day_night.shape[0], 1), axis = 1).transpose()
 	data_series = np.array([data_series_1, data_series_3, data_series_4, data_series_5])
+	#print('day data: ', day_count)
+	#print('night data: ', night_count)
 	return data_series
 	
 def get_wakes():
